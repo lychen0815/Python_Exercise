743aec3e2775f486c7fa327c8a5f96d80b15b5cb
liuyuchen <change file framework>
2021-08-17 01:39:06 +1000

    add GE02_Q01 file

---
+++ lib/Programming Bootcamp/Group_Exercises_2_Control_Statements_Boolean_Logic/GE02_Q01.py
@@ -0,0 +1,6 @@
+"""
+Consider a very primitive login system.
+Create a program that
+a.Asks the user for a username and password
+Only allows access if the username is “admin” and the password is “hunter2” (e.g. prints “Access Granted” or “Access Denied”)
+"""
