c39a495a3a42aa4fcb2600bf9ad5a1bd5b2f1a75
liuyuchen <change file framework>
2021-08-17 00:05:42 +1000

    add GE01_Q03 file

---
+++ lib/Programming Bootcamp/Group_Exercises_1_Variables_Syntax_Arithmetic/GE01_Q03.py
@@ -0,0 +1,6 @@
+"""
+Write a program that
+a.Asks the user for their name (e.g. “What is your name?” “John”).
+b.Asks the user for their age, using their name (e.g. “How old are you, John?” “18”)
+c.Display a message to them telling them how old they’ll be in 10 years (e.g. “You will be 28 in 10 years”)
+"""
