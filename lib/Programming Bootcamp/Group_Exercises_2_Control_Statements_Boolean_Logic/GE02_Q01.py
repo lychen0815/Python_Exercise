836db2ecd06d3616d5b14bcfac622e8173546571
liuyuchen <change file framework>
2021-08-18 00:17:56 +1000

    add GE02_Q02 file and update GE02_Q01 file

--- lib/Programming Bootcamp/Group_Exercises_2_Control_Statements_Boolean_Logic/GE02_Q01.py
+++ lib/Programming Bootcamp/Group_Exercises_2_Control_Statements_Boolean_Logic/GE02_Q01.py
@@ -4,3 +4,16 @@ Create a program that
 a.Asks the user for a username and password
 Only allows access if the username is “admin” and the password is “hunter2” (e.g. prints “Access Granted” or “Access Denied”)
 """
+
+##part(a)
+
+#put username and password
+log_username = input("please enter a username >>>")
+log_password = input("please enter a password >>>")
+
+##part(b)
+if(log_username == "admin" and log_password == "hunter2"):
+    print("Access Granted!")
+else:
+    print("Error username or password !! Please try again")
+
---
+++ lib/Programming Bootcamp/Group_Exercises_2_Control_Statements_Boolean_Logic/GE02_Q02.py
@@ -0,0 +1,10 @@
+"""
+At university, we have a grading system where the mark you receive determines the grade you get. The system is as follows:
+N	0 - 49
+P	50 - 59
+C	60 - 69
+D	70 - 79
+HD	80 - 100
+Write a program that gets a numeric mark from the user and returns the respective grade.
+"""
+
