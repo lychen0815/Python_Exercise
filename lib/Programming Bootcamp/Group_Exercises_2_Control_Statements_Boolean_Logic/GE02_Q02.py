aa2b8375cf75ba14b4f0ebbc2d35485ab7e2c082
liuyuchen <change file framework>
2021-08-18 00:32:39 +1000

    add GE02_Q02 file and update GE02_Q01 file

--- lib/Programming Bootcamp/Group_Exercises_2_Control_Statements_Boolean_Logic/GE02_Q02.py
+++ lib/Programming Bootcamp/Group_Exercises_2_Control_Statements_Boolean_Logic/GE02_Q02.py
@@ -8,3 +8,15 @@ HD	80 - 100
 Write a program that gets a numeric mark from the user and returns the respective grade.
 """
 
+user_mark = int(input("please enter your numeric mark >>>"))
+
+if user_mark >= 80:
+    print("Your grade is HD")
+elif user_mark >= 70:
+    print("Your grade is D")
+elif user_mark >= 60:
+    print("Your grade is C")
+elif user_mark >= 50:
+    print("Your grade is P")
+else:
+    print("Your grade is N")
