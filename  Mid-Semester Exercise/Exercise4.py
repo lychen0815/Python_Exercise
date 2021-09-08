6da5cc9fc0679c0bb9c5593028519ae2c5b915e3
lychen <yuchenliu0815@gmail.com>
2021-09-08 23:53:19 +1000

    change Exercise 4

---
+++  Mid-Semester Exercise/Exercise4.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, name, organisation, phones=[]):
+        self.name = name
+        self.organisation = organisation
+        self.phones = phones
+
+
+
+class PhoneBook:
