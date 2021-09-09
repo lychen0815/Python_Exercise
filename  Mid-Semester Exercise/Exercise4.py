392c8466e719da8f931d7dc2c38e5514e8817e67
lychen <yuchenliu0815@gmail.com>
2021-09-09 23:28:14 +1000

    delete Exercise4

---  Mid-Semester Exercise/Exercise4.py
+++
@@ -1,9 +0,0 @@
-class Contact:
-    def __init__(self, name, organisation, phones=[]):
-        self.name = name
-        self.organisation = organisation
-        self.phones = phones
-
-
-
-class PhoneBook:
