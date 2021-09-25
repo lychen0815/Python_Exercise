87d2b3e2a7891aa02289f470d7fc5bab375d635a
lychen <yuchenliu0815@gmail.com>
2021-09-25 14:02:36 +1000

    改变task2的版本

--- Final-Semester-Exercise/A/task4.py
+++ Final-Semester-Exercise/A/task4.py
@@ -1,15 +1,15 @@
-import re
 
 file = open('32009682_clean_dialogue.txt','r')
 
 data = file.read()
 
+#将文件读出的字符串形式转换为list的形式
 dialogue_list = eval(data)
 
+#将文件转换为人能读的懂的格式
 new_dialogue = ""
 
 for i in dialogue_list:
     new_tuple = "".join(i)
     new_dialogue += (new_tuple + "\n")
 print(new_dialogue)
-
