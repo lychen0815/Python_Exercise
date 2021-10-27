a86e6465611c9c0b9f76f288f4d08e58eb59ac76
lychen <yuchenliu0815@gmail.com>
2021-10-27 20:08:29 +1100

    add Q1 in mock

---
+++ Final-Semester-Exercise/Mock/Q1.py
@@ -0,0 +1,7 @@
+"""
+Q1: This question is about Classes and Objects.
+Create a class named Bank, which has attributes Account Number, Account Name, Balance, Address. It will be performing transactions between 2 accounts, checking the balance, withdrawing, changing the account name, and passbook. Complete the following code template for the Bank class:
+
+Transaction - removes funds from the bank account and sends it to another account. Check Balance - check the balance of the account holder. Withdraw - withdraws the amount from the account holder, and if withdrawal amount is higher than balance it should be rejected (return False). Change Name - should allow users to change the name. Passbook - should print all the current details of account holder into a file.
+"""
+
