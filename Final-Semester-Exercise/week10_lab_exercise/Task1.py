1337575c5f2e4a4920e177650465c20ec56e690d
lychen <yuchenliu0815@gmail.com>
2021-10-31 22:39:38 +1100

    add new exercise in week10_lab

---
+++ Final-Semester-Exercise/week10_lab_exercise/Task1.py
@@ -0,0 +1,8 @@
+def linear_search_tuples(a_list,query):
+    for index, element in a_list:
+        if element == query:
+            return True
+    return False
+
+print(linear_search_tuples([(1,'a'),(2,'c'),('abc','ee')],'c'))
+print(linear_search_tuples([(1,'a'),(2,'c'),('abc','ee')],'abc'))
